f9ebd688b89586d38a91b8bb248f8a313acca639
Philip Clement <pclement803@gmail.com>
2020-01-21 23:41:34 -0600

    minor bug fixes

--- src/setDataBase.js
+++ src/setDataBase.js
@@ -5,12 +5,16 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import "firebase/database";
 
 const setDataBase = ({ userState, new_cart }) => {
+  if (userState.user) {
     let id = userState.user.displayName;
     let db = firebase.database().ref();
-    let user_attributes
+    let user_attributes;
     if (new_cart === undefined) {
+      let new_cart= []
+      console.log('hello')
       user_attributes = {
-        name: id
+        name: id,
+        cart: new_cart
       };
     } else {
       user_attributes = {
@@ -21,6 +25,7 @@ const setDataBase = ({ userState, new_cart }) => {
     db.child("Users")
       .child(id)
       .set(user_attributes);
-  };
+  }
+};
 
-  export default setDataBase
+export default setDataBase;
